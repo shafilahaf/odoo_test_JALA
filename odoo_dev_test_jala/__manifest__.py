8d4abce33687d67fd60632b38c4371d5cea9d0ef
Shafilah AF <61927381+shafilahaf@users.noreply.github.com>
2025-03-01 12:47:20 +0700

    wip: Pengembangan Modul Kustom - Task 1

--- odoo_dev_test_jala/__manifest__.py
+++ odoo_dev_test_jala/__manifest__.py
@@ -1,32 +1,23 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "odoo_dev_test_jala",
+    'name': "Odoo Dev Test Jala",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
-
-    'description': """
-        Long description of module's purpose
+        This module is created for the purpose of Odoo Development Test by JALA.
     """,
 
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
+    'description': """
+        This module is created for the purpose of Odoo Development Test by JALA.
+    """,
 
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
+    'author': "SHAFILAH AF",
     'category': 'Uncategorized',
     'version': '0.1',
-
-    # any module necessary for this one to work correctly
-    'depends': ['base'],
-
-    # always loaded
+    'depends': ['base', 'mail'],
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'security/ir.model.access.csv',
+        'data/project_mgt_mail_template.xml',
+        'views/project_management.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
---
+++ odoo_dev_test_jala/models/project_management.py
@@ -0,0 +1,49 @@
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError, ValidationError
+
+class ProjectMgt(models.Model):
+    _name = 'project.management'
+    _rec_name = 'project_name'
+    _description = 'Project Management'
+
+    project_name = fields.Char(string="Nama Proyek", required=True)
+    description = fields.Text(string="Deskripsi")
+    start_date = fields.Date(string="Tanggal Mulai", required=True)
+    end_date = fields.Date(string="Tanggal Selesai", required=True)
+    state = fields.Selection([('aktif', 'Aktif'), ('selesai', 'Selesai'), ('ditangguhkan', 'Ditangguhkan')], string="Status", default='aktif')
+    
+    # Shafilah - Add new field
+    duration = fields.Integer(string="Durasi", compute="_compute_duration", store=True)
+    project_owner = fields.Many2one('res.users', string="Pemilik Proyek", required=True)
+
+    def write(self, vals):
+        """
+        Send an email to project owner when the state project is changed.
+        """
+        for rec in self:
+            old_state = rec.state
+            new_state = vals.get('state', old_state)
+
+            # Check if the state is changed
+            res = super(ProjectMgt, self).write(vals)
+
+            if old_state != new_state:
+                mail_template_notif = self.env.ref('odoo_dev_test_jala.jala_project_mgt_notif_status_mail_template')
+                if mail_template_notif:
+                    mail_template_notif.send_mail(rec.id, force_send=True)
+
+            return res
+
+    @api.depends('start_date', 'end_date')
+    def _compute_duration(self):
+        """
+        Compute the duration of the project in days, based on the start_date and end_date
+        """
+        for rec in self:
+            if rec.start_date and rec.end_date:
+                rec.duration = (rec.end_date - rec.start_date).days
+                rec.duration = max(rec.duration, 0)
+            else:
+                rec.duration = 0
+
+    
